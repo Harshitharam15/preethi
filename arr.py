998b86910b90bd715af3c9cf879e6b8cd7314af0
Harshitharam15 <136304565+Harshitharam15@users.noreply.github.com>
2023-11-20 09:59:35 +0530

    main

---
+++ arr.py
@@ -0,0 +1,8 @@
+import numpy as np
+arr1=np.array([1,2,3])
+arr2=np.array([[1.8,2.5,3.14],[4,5,6]])
+arr3=np.array([[1,2,3],[4,5,6],[7,8,9]])
+print('datatype of an array')
+print('datatype of array1:',arr1.dtype)
+print('datatype of array2:',arr2.dtype)
+print('datatype of array3:',arr3.dtype)
